b25b90ce196aa21a6d7171461957e6304fec5f9c
Victor Melnyk <victor.melnyk.vm@gmail.com>
2016-03-08 20:38:23 +0200

    update project structure

--- Run.cmd
+++
@@ -1,3 +0,0 @@
-start cmd /k grunt
-start cmd /k npm start
-start cmd /k nodemon src/index.js
--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('./scripts/_tools.js');
-require('./scripts/_start.js');
-
-require('./scripts/classes.js');
-require('./scripts/let.js');
-require('./scripts/const.js');
-
-require('./scripts/_finish.js');
