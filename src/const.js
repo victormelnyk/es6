e1eb397f16a7486411e5061dd4f3ec4bf5f9275f
Victor Melnyk <victor.melnyk.vm@gmail.com>
2016-03-08 22:33:41 +0200

    add const example

--- src/const.js
+++ src/const.js
@@ -1,2 +1,26 @@
 'use strict';
 h.logHeader('Const');
+
+// test 1
+const value1 = 1;
+h.l(value1);
+
+try {
+  value1 = 2;
+} catch (error) {
+  h.l(error); // TypeError: Assignment to constant variable.
+}
+
+// test 2
+const user = {
+  name: 'Bob'
+};
+
+user.name = 'Ted';
+h.l(user.name);
+
+try {
+  user = {};
+} catch (error) {
+  h.l(error); // TypeError: Assignment to constant variable.
+}
